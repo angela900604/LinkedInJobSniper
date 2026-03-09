677aad487e666a860a7980db245a2447c069b8ea
Marofuego <marofuego@gmail.com>
2026-03-08 20:33:03 -0700

    Update search terms and locations in main.py

--- main.py
+++ main.py
@@ -38,15 +38,27 @@ load_dotenv()
 # Configuration
 # Support multiple keywords (each term will be searched and then results will be merged & de-duplicated).
 
+# SEARCH_TERMS = [
+#    "Data Engineer",
+#    "Analytics Engineer",
+#    "AI Engineer",
+#    "Machine Learning Engineer",
+#    "Data Platform Engineer",
+#    "Platform Engineer"
+#]
+# LOCATIONS = ["Canada", "Toronto, Canada", "Toronto, ON"]
+
 SEARCH_TERMS = [
-    "Data Engineer",
-    "Analytics Engineer",
-    "AI Engineer",
-    "Machine Learning Engineer",
-    "Data Platform Engineer",
-    "Platform Engineer"
+    "UI Designer",
+    "UX Designer",
+    "Product Designer",
+    "User Experience Designer",
+    "UI/UX Designer",
+    "User Interface Designer"
 ]
-LOCATIONS = ["Canada", "Toronto, Canada", "Toronto, ON"]
+LOCATIONS = ["Canada", "Toronto, ON", "Vancouver, BC"]
+
+
 RESULT_LIMIT = 40
 HOURS_OLD = 72
 PROXY_URL = os.getenv("PROXY_URL", None)
