14e310dfae88dfba3bb4025693eb3fe9ac6c6679
Marofuego <marofuego@gmail.com>
2026-03-09 08:04:40 -0700

    Update main.py

--- main.py
+++ main.py
@@ -38,25 +38,25 @@ load_dotenv()
 # Configuration
 # Support multiple keywords (each term will be searched and then results will be merged & de-duplicated).
 
-# SEARCH_TERMS = [
-#    "Data Engineer",
-#    "Analytics Engineer",
-#    "AI Engineer",
-#    "Machine Learning Engineer",
-#    "Data Platform Engineer",
-#    "Platform Engineer"
-#]
-# LOCATIONS = ["Canada", "Toronto, Canada", "Toronto, ON"]
-
-SEARCH_TERMS = [
-    "UI Designer",
-    "UX Designer",
-    "Product Designer",
-    "User Experience Designer",
-    "UI/UX Designer",
-    "User Interface Designer"
+ SEARCH_TERMS = [
+    "Data Engineer",
+    "Analytics Engineer",
+    "AI Engineer",
+    "Machine Learning Engineer",
+    "Data Platform Engineer",
+    "Platform Engineer"
 ]
-LOCATIONS = ["Canada", "Toronto, ON", "Vancouver, BC"]
+LOCATIONS = ["Canada", "Toronto, Canada", "Toronto, ON"]
+
+# SEARCH_TERMS = [
+#    "UI Designer",
+#    "UX Designer",
+#    "Product Designer",
+#    "User Experience Designer",
+#    "UI/UX Designer",
+#    "User Interface Designer"
+#]
+#LOCATIONS = ["Canada", "Toronto, ON", "Vancouver, BC"]
 
 
 RESULT_LIMIT = 40
